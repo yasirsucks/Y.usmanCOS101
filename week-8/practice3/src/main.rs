d70cd1cf5366c3c0986444b7bd294c85ed04f98c
not-fazed <muhammed-faiz.oyebanji@pau.edu.ng>
2025-11-19 13:44:43 +0100

    using get method

---
+++ week-8/practice3/src/main.rs
@@ -0,0 +1,23 @@
+// Method to print the get value
+fn value(n:Option<&char>)
+{
+
+println!("Element of vector { :? }",n);
+}
+fn main() {
+
+let v = vec! ['R' , 'U', 'S' , 'T' ,'A' , 'C' ,'I' , 'A' , 'N' ];
+
+let mut input1 = String :: new();
+println!("\nEnter an index value btw (0 - 8)");
+std :: io :: stdin().read_line(&mut input1).expect("Failed to read input");
+
+//index is the non negative value which is smaller than the size of the vector
+let index:usize = input1.trim().parse().expect("Invalid input");
+
+// getting value at given index value
+let ch: Option<&char> = v.get(index);
+value(ch);
+
+
+}
