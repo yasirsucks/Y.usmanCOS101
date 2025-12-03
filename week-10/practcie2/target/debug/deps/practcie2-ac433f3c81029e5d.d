4f3e3a34aa5548796f3d7b6bf84ffb3cc4c79aa5
not-fazed <muhammed-faiz.oyebanji@pau.edu.ng>
2025-12-03 13:10:13 +0100

    assigning value of one variable to another variable

---
+++ week-10/practcie2/target/debug/deps/practcie2-ac433f3c81029e5d.d
@@ -0,0 +1,5 @@
+C:\Users\danni\Documents\Y.usmanCOS101\week-10\practcie2\target\debug\deps\practcie2-ac433f3c81029e5d.d: src\main.rs
+
+C:\Users\danni\Documents\Y.usmanCOS101\week-10\practcie2\target\debug\deps\practcie2-ac433f3c81029e5d.exe: src\main.rs
+
+src\main.rs:
---
+++ week-10/practcie2/target/debug/practcie2.d
@@ -0,0 +1,1 @@
+C:\Users\danni\Documents\Y.usmanCOS101\week-10\practcie2\target\debug\practcie2.exe: C:\Users\danni\Documents\Y.usmanCOS101\week-10\practcie2\src\main.rs
