2c6555fb8e586129a8b9852e1e265b5ee7209695
not-fazed <muhammed-faiz.oyebanji@pau.edu.ng>
2025-12-03 13:22:23 +0100

    mutable refrences

--- week-10/practice6/src/main.rs
+++ week-10/practice6/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    // a list of numbers
+    let x = vec![100, 200, 300];
+    borrow_vector(&x); // passing reference
+
+    println!("Printing the value from main() x[0] = {}", x[0]);
+    println!("****************************");
+}
+
+fn borrow_vector(z: &Vec<i32>) {
+    println!("****************************");
+    println!("Inside print_vector_function {:?}\n", z);
+    println!("----------------------------");
 }
