976179f9373d72d8af46c03e99ba76871cc4b9f3
not-fazed <muhammed-faiz.oyebanji@pau.edu.ng>
2025-12-03 13:16:28 +0100

    picking a variable in the vector

---
+++ week-10/practice4/src/main.rs
@@ -0,0 +1,14 @@
+fn main(){
+
+// a list of nos
+let v = vec![15, 25, 35, 45, 55];
+print_vector(v);
+println! ("{}",v[ø]); // this line gives error
+
+fn print_vector(x:Vec<i32>){
+
+println!("Inside print_vector function { :? }",x);
+
+}
+
+}
